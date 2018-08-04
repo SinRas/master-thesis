ac69ec3be11b2d7a127dea33b4635f9ee40d850f
Unknown <rasoolibox193@gmail.com>
2018-08-04 18:35:56 +0430

    tidy styles and commands and add problems definition
    separated files containing styles and commands.
    added problem description in introduction.

---
+++ commands.cmd
@@ -0,0 +1,14 @@
+% ------ Path to chapter files ----- %
+\newcommand*{\chapters}{\basepath chapters/}
+
+% ------ SubSubSubSection ----- %
+\newcommand{\subsubsubsection}[1]{{\bf #1\par}}
+
+
+% ------ Math Operations ----- %
+\newcommand{\randomvariable}[1]{\mathfrak{#1}}
+\newcommand{\set}[1]{\mathcal{#1}}
+\newcommand{\hollow}[1]{\mathbb{#1}}
+\newcommand{\mthfnc}[1]{ \mathit{#1} }
+\newcommand{\simplex}[1]{ \Delta^{#1-1} = \left\lbrace x \in \hollow{R}_+^{#1} \; : \; \sum^{#1}_{i=1} x_i = 1 \right\rbrace }
+
